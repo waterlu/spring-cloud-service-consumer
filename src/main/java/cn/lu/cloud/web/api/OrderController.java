b862e259a7aac3dd54e2f1c185f98be0bb82e641
鲁铁华 <lutiehua@zj-inv.cn>
2017-10-25 11:48:50 +0800

    支持zipkin

--- src/main/java/cn/lu/cloud/web/api/OrderController.java
+++ src/main/java/cn/lu/cloud/web/api/OrderController.java
@@ -6,6 +6,8 @@ import cn.lu.cloud.data.OrderData;
 import cn.lu.cloud.dto.CreateOrderDTO;
 import cn.lu.cloud.dto.ProductDTO;
 import cn.lu.cloud.entity.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +20,16 @@ import java.math.BigDecimal;
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    private final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private ProductClient productClient;
 
     @PostMapping("/")
     public ResponseResult createOrder(@RequestBody CreateOrderDTO createOrderDTO) {
+
+        logger.info("createOrder {}", createOrderDTO);
+
         int errorCode = 1001;
         String errorMessage = "生成订单失败";
 
